b6a646b312d9183b4f7f2287c13b1626fb70c159
Sara Eide-Altman <SaraRose686@gmail.com>
2019-05-28 00:25:38 -0700

    Updated functionality

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 "use strict";
 
+let TITLE = {
+    first: [" computer", " software", " web", " front-end", " back-end", " full-stack", "n application"],
+    firstPos: 0,
+    second: ["programmer", "developer", "engineer"],
+    secondPos: 0,
+    nextFirstTitle: () => TITLE.first[ (TITLE.firstPos++ % TITLE.first.length) ],
+    nextSecondTitle: () => TITLE.second[ (TITLE.secondPos++ % TITLE.second.length) ]
+};
 
+function handleSendEmailMessage() {
 
+}
+
+function handleMenuExpandCollapse() {
+
+}
+
+function handleTitleTransition() {
+    $("#titleFirst").fadeOut("slow", () => {
+        $("#titleFirst").text(TITLE.nextFirstTitle()).fadeIn();
+    });
+
+    $("#titleSecond").fadeOut("slow", () => {
+        $("#titleSecond").text(TITLE.nextSecondTitle()).fadeIn();
+    });
+}
+
+function runPageEffects() {
+    setInterval(handleTitleTransition, 3000);
+    handleMenuExpandCollapse();
+    handleSendEmailMessage();
+}
+
+$(runPageEffects);
